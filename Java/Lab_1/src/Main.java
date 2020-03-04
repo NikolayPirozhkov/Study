533c22dc49954bc631c6d5ceced8d3a847b226aa
Nikolay Pirozhkov <pirnv@list.ru>
2020-03-04 13:19:01 +0300

    Lab 1 Solution

--- Java/Lab_1/src/Main.java
+++ Java/Lab_1/src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
+/*
+Реализуйте метод boolean swap(int i, int j, int[] anArray),
+меняющий местами элементы по индексам i и j в массиве anArray
+ и возвращающий false, если индексы выходят за пределы массива
+*/
 
+public class Main {
+    boolean swap(int i, int j, int[] anArray){
+        if(i < anArray.length || j < anArray.length){
+            int temp = anArray[i];
+            anArray[i] = anArray[j];
+            anArray[j] = temp;
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
         // write your code here
     }
