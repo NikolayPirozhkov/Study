b5a3ce3f2fd380695c8b165f21afdec18ad3e8bc
Nikolay Pirozhkov <pirnv@list.ru>
2020-03-20 15:53:31 +0300

    lab_3 refactoring lab_4 add

---
+++ Java/Lab_5/src/UsualCalc.java
@@ -0,0 +1,10 @@
+/**
+ * @author NikolayPirozhkov
+ * @project Java
+ */
+public class UsualCalc implements ICalculator {
+    @Override
+    public int summ(int x, int y) {
+        return x+y;
+    }
+}
