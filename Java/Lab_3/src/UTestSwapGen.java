faed7fcd448ece808e98904dace7b55971d3ed31
Nikolay Pirozhkov <pirnv@list.ru>
2020-05-11 15:51:22 +0300

    refactoring lab 3

--- Java/Lab_3/src/UTestSwapGen.java
+++ Java/Lab_3/src/UTestSwapGen.java
@@ -14,10 +14,10 @@ class UTestSwapGen {
     @org.junit.jupiter.api.Test
     void swap() {
         TSwap tester = new TSwap();
-        String [] testStringArr = {"hallo","World","f*king"};
+        String [] testStringArr = {"hallo","World","Beautiful"};
         tester.swap(testStringArr,1,2);
         List<String> testList = new ArrayList<>(Arrays.asList(testStringArr));
         tester.swap(testList,1,2);
-        assertArrayEquals(testList.toArray(), new String [] {"hallo","World","f*king"},"start message == message after double swap");
+        assertArrayEquals(testList.toArray(), new String [] {"hallo","World","Beautiful"},"start message == message after double swap");
     }
 }
